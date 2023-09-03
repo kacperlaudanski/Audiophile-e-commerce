a72de66074dfafc1112788bf63f5aa6c8c57aa1c
Kacper <kacper.laudanski@wp.pl>
2023-09-03 20:03:39 +0200

    product page, product cards logic finished

--- my-app/src/components/Categories/Speakers/SpeakersPage.tsx
+++ my-app/src/components/Categories/Speakers/SpeakersPage.tsx
@@ -7,8 +7,7 @@ import ProductPreview from "../../Elements/Products/ProductPreview";
 import CategorySelectionPanel from "../../Home/CategorySelectionPanel";
 import AboutCompany from "../../Home/AboutCompany";
 import Footer from "../../Elements/Footer/Footer";
-import ZX9SpeakerImage from "../../../images/product-zx9-speaker/desktop/image-category-page-preview.jpg";
-import ZX7SpeakerImage from '../../../images/product-zx7-speaker/desktop/image-category-page-preview.jpg'; 
+import { speakersList } from "../../Elements/Products/ProductLists";
 import "../../../dist-styles/categories.css";
 
 const SpeakersPage: React.FC = (props) => {
@@ -19,20 +18,17 @@ const SpeakersPage: React.FC = (props) => {
         <CategoryBanner>SPEAKERS</CategoryBanner>
       </HeaderSection>
       <MainSection>
-        <ProductPreview
-          productImage={ZX9SpeakerImage}
-          productName="ZX9 SPEAKER"
-          productDescription="Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups."
-          productPageLink="#"
-          reverseClass={null}
-        />
-        <ProductPreview
-          productImage={ZX7SpeakerImage}
-          productName="ZX7 SPEAKER"
-          productDescription="Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use."
-          productPageLink="#"
-          reverseClass='reverse'
-        />
+        {speakersList.map((speaker) => {
+          return (
+            <ProductPreview 
+              productImage={speaker.image}
+              productName={speaker.name}
+              productDescription={speaker.description}
+              productPageLink={speaker.link}
+              reverseClass={speaker.productPreviewReverse}
+            />
+          )
+        })}
         <CategorySelectionPanel /> 
         <AboutCompany />
       </MainSection>

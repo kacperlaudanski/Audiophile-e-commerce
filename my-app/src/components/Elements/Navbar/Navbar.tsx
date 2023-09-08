1fa5ebcc6be8b1c4a84a14519891e69b4da64e8b
Kacper <kacper.laudanski@wp.pl>
2023-09-08 19:46:35 +0200

    cart modal logic

--- my-app/src/components/Elements/Navbar/Navbar.tsx
+++ my-app/src/components/Elements/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import {useState} from 'react'
 import { NavLink } from "react-router-dom";
 import "../../../dist-styles/navbar.css";
+import CartModal from "../../Cart/CartModal";
 import CategoriesList from "../CategoriesElements/CategoriesList";
 
 const Navbar = () => {
+  const [modal, setModal] = useState(false); 
+
   return (
     <nav className="navbar-desktop">
       <div className="logo-container">
@@ -16,8 +19,12 @@ const Navbar = () => {
         linkClass = "navbar-list-element"
       />
       <div className="cart-button-container">
-        <button type="button" className="cart-button"></button>
+        <button type="button" className="cart-button" onClick={() => setModal(true)}></button>
       </div>
+      <CartModal 
+        closeModal={() => setModal(false)}
+        openModal= {modal}
+      >pdpd</CartModal>
     </nav>
   );
 };

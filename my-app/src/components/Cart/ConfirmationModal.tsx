9fc8cd520e4144a25e7fd7f6d9c5015088c0c59e
Kacper <kacper.laudanski@wp.pl>
2023-09-18 19:08:20 +0200

    small fixes

--- my-app/src/components/Cart/ConfirmationModal.tsx
+++ my-app/src/components/Cart/ConfirmationModal.tsx
@@ -1,26 +1,47 @@
-import React, {ReactEventHandler, useEffect, useRef} from 'react'
-import ConfirmationIcon from '../../images/checkout/icon-order-confirmation.svg'; 
-import '../../dist-styles/cart.css';
+import React, { ReactEventHandler, useEffect, useRef } from "react";
+import ConfirmationIcon from "../../images/checkout/icon-order-confirmation.svg";
+import "../../dist-styles/cart.css";
+import { NavLink } from "react-router-dom";
 
-const ConfirmationModal: React.FC<{closeModal: ReactEventHandler, openModal: boolean}> = ({closeModal, openModal}) => {
+const ConfirmationModal: React.FC<{
+  closeModal: ReactEventHandler;
+  openModal: boolean;
+}> = ({ closeModal, openModal }) => {
+  const ref = useRef<HTMLDialogElement | null>(null);
 
-const ref = useRef<HTMLDialogElement | null>(null)
+  useEffect(() => {
+    if (openModal) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [openModal]);
 
-useEffect(() => {
-  if(openModal){
-    ref.current?.showModal(); 
-  }else{
-    ref.current?.close(); 
-  }
-}, [openModal]);
+  return (
+    <dialog
+      className="confirmation-modal-container"
+      ref={ref}
+      onCancel={closeModal}
+    >
+      <img
+        src={ConfirmationIcon}
+        alt="confirmation-icon"
+        className="confirmation-icon"
+      ></img>
+      <h2>PRODUCT ADDED TO THE CART</h2>
+      <div className="confirmation-buttons-container">
+        <button
+          className="confirmation-continue-button"
+          type="button"
+          onClick={closeModal}
+        >
+          CONTINUE SHOPPING
+        </button>
+        <small>OR</small>
+        <NavLink to="/checkout" className='confirmation-checkout-button'>CHECKOUT</NavLink>
+      </div>
+    </dialog>
+  );
+};
 
-    return(
-        <dialog className='confirmation-modal-container' ref={ref} onCancel={closeModal}>
-            <img src={ConfirmationIcon} alt='confirmation-icon' className='confirmation-icon'></img>
-            <h2>PRODUCT ADDED TO THE CART</h2>
-            <button className='confirmation-continue-button' type='button' onClick={closeModal}>CONTINUE SHOPPING</button>
-        </dialog>
-    )
-}
-
-export default ConfirmationModal; 
+export default ConfirmationModal;

21d36d2cdcd3c003c61923759018d73aedb1e50d
Kacper <kacper.laudanski@wp.pl>
2023-10-07 11:47:12 +0200

    footer mobile version

--- my-app/src/components/Elements/Footer/Footer.tsx
+++ my-app/src/components/Elements/Footer/Footer.tsx
@@ -12,30 +12,37 @@ const Footer = () => {
           <h3 className="footer-logo">audiophile</h3>
           <p className="footer-description">
             Audiophile is an all in one stop to fulfill your audio needs. We're
-            a small team <br></br>
-            of music lovers and sound specialists who are devoted to helping you
-            get the <br></br>
-            most out of personal audio. Come and visit our demo facility - we're
-            open 7 <br></br>
-            days a week.
+            a small team of music lovers and sound specialists who are devoted
+            to helping you get the most out of personal audio. Come and visit
+            our demo facility - we're open 7 days a week.
           </p>
           <small>Copyright 2021. All Rights Reserved</small>
         </div>
         <div className="footer-nav-social-container">
           <nav>
-            <CategoriesNavList
-              listClass="footer-categories-list"
-            />
+            <CategoriesNavList listClass="footer-categories-list" />
           </nav>
           <div className="footer-socials">
             <a href="javascript:void(0)">
-              <img src={FacebookIcon} alt="facebook-icon" className="social-icon"></img>
+              <img
+                src={FacebookIcon}
+                alt="facebook-icon"
+                className="social-icon"
+              ></img>
             </a>
             <a href="javascript:void(0)">
-              <img src={TwitterIcon} alt="twitter-icon" className="social-icon"></img>
+              <img
+                src={TwitterIcon}
+                alt="twitter-icon"
+                className="social-icon"
+              ></img>
             </a>
             <a href="javascript:void(0)">
-              <img src={InstagramIcon} alt="instagram-icon" className="social-icon"></img>
+              <img
+                src={InstagramIcon}
+                alt="instagram-icon"
+                className="social-icon"
+              ></img>
             </a>
           </div>
         </div>

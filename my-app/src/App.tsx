cc425408f33c2b9454d9d03d5352239259aff3b8
Kacper <kacper.laudanski@wp.pl>
2023-10-13 23:24:13 +0200

    firebase hosting config  and small code improvement

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import HeadphonesPage from "./pages/Headphones/HeadphonesPage";
 import EarphonesPage from "./pages/Earphones/EarphonePage";
